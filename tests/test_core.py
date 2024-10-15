6c9711cd56724184dc246fd269af0fdaf8b925d0
Elisa Márquez Zavala <emarquez@lcg.unam.mx>
2024-10-15 13:48:03 +0200

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,11 +4,11 @@ from cutiepynb.core import cutipy_nb
 
 def test_cutipy_nb():
     # Path to the test notebook
-    test_file = '.docs/examples/Test.ipynb'
+    test_file = './docs/examples/Test.ipynb'
     cutipy_nb(test_file, colors=['#40498e', '#357ba3'], save=True)
 
     # Check that the modified file exists and has the expected changes
-    modified_file = './examples/Test_chulo.ipynb'
+    modified_file = './docs/examples/Test_chulo.ipynb'
     assert os.path.exists(modified_file)
 
     with open(modified_file, 'r') as f:
